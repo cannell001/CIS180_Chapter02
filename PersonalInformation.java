16d8b1ae57a65e508ddafddcccecde80996c7cc2
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-10 07:07:49 -0400

    LAB 2.3 Personal Info

---
+++ PersonalInformation.java
@@ -0,0 +1,17 @@
+/**
+ * Chapter 2
+ * Programming Challenge 3: Personal Information
+ */
+
+public class PersonalInformation
+{
+   public static void main(String[] args)
+   {
+      // Display the information.
+      System.out.println("Jill Johnson\n" +
+                         "123 Main Street\n" +
+                         "MyTown, NC  55555\n" +
+                         "(555)555-1212\n" +
+                         "Computer Science");
+   }
+}
